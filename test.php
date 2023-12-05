f8cba3d3698431b35f8bffdc8ce7c750b6dfa297
Björn Büttner <bjoern.buettner@jungwild.io>
2023-12-05 22:45:37 +0100

    add input challenge

--- test.php
+++ test.php
@@ -13,24 +13,39 @@ $type = array_pop($types);
 $keys = array_keys($words[$type]);
 shuffle($keys);
 
-$question = array_pop($keys);
-$wrong1 = array_pop($keys);
-$wrong2 = array_pop($keys);
-
-$answers = [$question, $wrong2, $wrong1];
-shuffle($answers);
-
 header('Mime-Type: application/json; utf-8');
-echo json_encode([
-    'answer' => array_search($question, $answers),
-    'answers' => array_map(function (string $key) use(&$type, &$words) {
-        if (is_array($words[$type][$key])) {
-            shuffle($words[$type][$key]);
-            return $words[$type][$key][0];
-        }
-        return $words[$type][$key];
-    }, $answers),
-    'question' => $question,
-]);
+if (rand(0, 1) === 1) {
+    $question = array_pop($keys);
+    $wrong1 = array_pop($keys);
+    $wrong2 = array_pop($keys);
+
+    $answers = [$question, $wrong2, $wrong1];
+    shuffle($answers);
+
+    echo json_encode([
+        'type' => 'multiple-choice',
+        'answer' => array_search($question, $answers),
+        'answers' => array_map(function (string $key) use(&$type, &$words) {
+            if (is_array($words[$type][$key])) {
+                shuffle($words[$type][$key]);
+                return $words[$type][$key][0];
+            }
+            return $words[$type][$key];
+        }, $answers),
+        'question' => $question,
+    ]);
+} else {
+    $answer = array_pop($keys);
+    $question = $words[$type][$answer];
+    if (is_array($question)) {
+        shuffle($question);
+        $question = $question[0];
+    }
+    echo json_encode([
+        'type' => 'input',
+        'answer' => $answer,
+        'question' => $question,
+    ]);
+}
 $datetime = date('Y-m-d H:i:s');
 file_put_contents(__DIR__ . '/log.txt', "$datetime {$_SERVER['REMOTE_ADDR']}\n", FILE_APPEND);
